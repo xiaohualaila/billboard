732e724913efecdfc859cf6b0151983475d8c64f
Jason <3648415@qq.com>
2018-09-10 11:11:19 +0800

    修改更新

--- app/src/main/java/cn/com/billboard/ui/TwoScreenActivity.java
+++ app/src/main/java/cn/com/billboard/ui/TwoScreenActivity.java
@@ -172,9 +172,7 @@ public class TwoScreenActivity extends XActivity<TwoScreenPresent> {
 
             float image_height = options.outHeight;
             float image_widht = options.outWidth;
-
-            Log.i("sss","image_height " +image_height + "  image_widht" + image_widht + "image_height/image_widht" + (image_height/image_widht));
-
+//            Log.i("sss","image_height " +image_height + "  image_widht" + image_widht + "image_height/image_widht" + (image_height/image_widht));
             if(image_height/image_widht > 1.2){
                 images_big.add(urls.get(i));
             }else {
