c226878b22eba62fad10074715639079694fe4b7
Administrator11 <380129462@qq.com>
2019-01-15 15:46:52 +0800

    修改

--- app/src/main/java/cn/com/billboard/ui/main/MainActivity.java
+++ app/src/main/java/cn/com/billboard/ui/main/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements AppDownload.Callb
         heartinterval();
 //        startService(new Intent(context, GPIOBigService.class));//两个电话四个按键
         //  startService(new Intent(context, GPIOBigService2.class));//一个电话四个按键
-        startService(new Intent(this, GPIOBigServiceNew.class));//采用了新的接线板子
+  //      startService(new Intent(this, GPIOBigServiceNew.class));//采用了新的接线板子
         getBus();
         instance = this;
 
@@ -89,8 +89,6 @@ public class MainActivity extends AppCompatActivity implements AppDownload.Callb
                 }
         );
     }
-
-
 
     TimerTask timerTask = new TimerTask() {
         @Override
