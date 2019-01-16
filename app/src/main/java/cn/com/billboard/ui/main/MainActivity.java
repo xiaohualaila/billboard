8b1e869942118bbba9aa7bf7ec9c1364b262bf78
Administrator11 <380129462@qq.com>
2019-01-16 16:34:12 +0800

    修改

--- app/src/main/java/cn/com/billboard/ui/main/MainActivity.java
+++ app/src/main/java/cn/com/billboard/ui/main/MainActivity.java
@@ -102,7 +102,6 @@ public class MainActivity extends AppCompatActivity implements AppDownload.Callb
      */
     private void heartinterval() {
         int time =  SharedPreferencesUtil.getInt(this, "time", 10);
-        time=2;
         mDisposable = Flowable.interval(0, time, TimeUnit.MINUTES)
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(aLong -> {
