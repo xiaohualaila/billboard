065b7db4be849615fa45be795adc9c74b43ac3a4
Administrator11 <380129462@qq.com>
2019-01-29 13:41:26 +0800

    修改

--- app/src/main/java/cn/com/billboard/ui/main/MainActivity.java
+++ app/src/main/java/cn/com/billboard/ui/main/MainActivity.java
@@ -67,7 +67,7 @@ public class MainActivity extends AppCompatActivity implements AppDownload.Callb
         heartinterval();
 //        startService(new Intent(context, GPIOBigService.class));//两个电话四个按键
         //  startService(new Intent(context, GPIOBigService2.class));//一个电话四个按键
-  //      startService(new Intent(this, GPIOBigServiceNew.class));//采用了新的接线板子
+        startService(new Intent(this, GPIOBigServiceNew.class));//采用了新的接线板子
         getBus();
         instance = this;
 
@@ -150,7 +150,7 @@ public class MainActivity extends AppCompatActivity implements AppDownload.Callb
         smdt.smdtWatchDogEnable((char) 0);
         //  stopService(new Intent(this, GPIOBigService.class));
         //  stopService(new Intent(this, GPIOBigService2.class));
-//        stopService(new Intent(this, GPIOBigServiceNew.class));
+        stopService(new Intent(this, GPIOBigServiceNew.class));
         if (mDisposable != null) {
             mDisposable.dispose();
         }
