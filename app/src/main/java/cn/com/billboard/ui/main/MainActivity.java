044cb9c9b96a1ec72407128b2230fb25fb041838
Administrator11 <380129462@qq.com>
2019-03-05 15:24:42 +0800

    Nothing

--- app/src/main/java/cn/com/billboard/ui/main/MainActivity.java
+++ app/src/main/java/cn/com/billboard/ui/main/MainActivity.java
@@ -73,7 +73,7 @@ public class MainActivity extends AppCompatActivity implements AppDownload.Callb
 
     private static final int REQUEST_CODE_MAIN = 999;
     private String account ="1023007213@qq.com";
-    private String call_account ="13289895424";//a716904256 设备
+    private String call_account ="13289895424";//a716904256 设备  1：手机  2： 设备
 //    private String account ="13289895424";
 //    private String call_account ="1023007213@qq.com";
     private Handler mhandler = new Handler();
